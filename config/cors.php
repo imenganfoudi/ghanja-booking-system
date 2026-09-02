86d29424675328cd013af5c99e03b19c682407de
imen <imenganfoudi123@gmail.com>
2026-09-02 13:28:48 +0200

    Add production frontend URL to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://ghanja-frontend.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
